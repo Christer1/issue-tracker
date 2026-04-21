15ad1f93a50a32990a6df8c7cfc7372c70f21492
Christer1 <yussupholawale2017@gmail.com>
2026-04-21 02:53:05 +0100

    feat:handle errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,39 +1,57 @@
-"use client"
-import { TextField, Button } from '@radix-ui/themes'
-import SimpleMDE from 'react-simplemde-editor';
+"use client";
+import { TextField, Button } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
-import {useForm, Controller} from 'react-hook-form';
-import axios from 'axios';
-import { useRouter } from 'next/navigation';
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Callout } from "@radix-ui/themes";
 
 interface IssueForm {
-    title: string;
-    description: string;
+  title: string;
+  description: string;
 }
 
 const NewIssuePage = () => {
-    const router = useRouter();
-    const {register, control, handleSubmit} = useForm<IssueForm>();
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState("");
   return (
-    <form 
-    className='max-w-xl space-y-3' onSubmit={handleSubmit(async (data) => {
-        await axios.post('/api/issues', data);
-        router.push('/issues');
-    })}>
-<TextField.Root placeholder="Title" {...register("title")}>
-	<TextField.Slot>
-		{/* <MagnifyingGlassIcon height="16" width="16" /> */}
-	</TextField.Slot>
-</TextField.Root>
-<Controller
-    name="description"
-    control={control}
-    render={({ field }) => <SimpleMDE {...field} placeholder="Description" />}
-/>
-<Button>Submit New Issue</Button>
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
 
-    </form>
-  )
-}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("An unexpected error occurred");
+          }
+        })}
+      >
+        <TextField.Root placeholder="Title" {...register("title")}>
+          <TextField.Slot>
+            {/* <MagnifyingGlassIcon height="16" width="16" /> */}
+          </TextField.Slot>
+        </TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE {...field} placeholder="Description" />
+          )}
+        />
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
+  );
+};
 
-export default NewIssuePage
+export default NewIssuePage;
