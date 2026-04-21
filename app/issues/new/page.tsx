82c2067b8a069f14f2160a9b18edf5bae54f0072
Christer1 <yussupholawale2017@gmail.com>
2026-04-21 02:41:42 +0100

    feat: implement issue creation by adding axios and routing to the new issue page

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -3,6 +3,8 @@ import { TextField, Button } from '@radix-ui/themes'
 import SimpleMDE from 'react-simplemde-editor';
 import "easymde/dist/easymde.min.css";
 import {useForm, Controller} from 'react-hook-form';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
 
 interface IssueForm {
     title: string;
@@ -10,10 +12,14 @@ interface IssueForm {
 }
 
 const NewIssuePage = () => {
+    const router = useRouter();
     const {register, control, handleSubmit} = useForm<IssueForm>();
   return (
     <form 
-    className='max-w-xl space-y-3' onSubmit={handleSubmit((data) => console.log(data))}>
+    className='max-w-xl space-y-3' onSubmit={handleSubmit(async (data) => {
+        await axios.post('/api/issues', data);
+        router.push('/issues');
+    })}>
 <TextField.Root placeholder="Title" {...register("title")}>
 	<TextField.Slot>
 		{/* <MagnifyingGlassIcon height="16" width="16" /> */}
